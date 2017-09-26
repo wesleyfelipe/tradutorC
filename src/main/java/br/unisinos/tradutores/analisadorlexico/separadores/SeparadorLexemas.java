e823667267e76042ccd993652d9679e6ccb9ca49
Wesley F <Wesley F>
2017-09-25 23:07:15 -0300

    Refactoring

--- src/main/java/br/unisinos/tradutores/analisadorlexico/separadores/SeparadorLexemas.java
+++ src/main/java/br/unisinos/tradutores/analisadorlexico/separadores/SeparadorLexemas.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class SeparadorLexemas {
 
 	private static String[] SEPARADORES = { "#", ",", ";", "(", ")", "{", "}", "\n", "\r", "\t", " ", "\"", "\'", ":",
-			"=", "+", "-", "%", "!", "&", "|" };
+			"=", "+", "-", "%", "!", "&", "|", ">", "<" };
 
 	public static List<String> separarLexemas(String code) {
 		List<String> lexemas = new ArrayList<String>();
@@ -20,41 +20,41 @@ public class SeparadorLexemas {
 			if (isCharacterAspas(ch)) {
 				entreAspas = !entreAspas;
 				pilha += ch;
-				
+
 			} else if (entreAspas) {
 				pilha += ch;
-				
+
 			} else if (isSeparador(ch.toString())) {
-				
+
 				if (!pilha.isEmpty())
 					lexemas.add(pilha);
 				if (!isCharacterIgnorado(ch))
 					lexemas.add(ch.toString());
 				pilha = new String();
-				
+
 			} else {
 				pilha += isCharacterEspaco(ch) ? "" : ch;
 			}
 		}
-		
-		if(!pilha.isEmpty())
+
+		if (!pilha.isEmpty())
 			lexemas.add(pilha);
 
 		return lexemas;
 	}
-	
-	protected static Boolean isCharacterAspas(Character ch){
+
+	protected static Boolean isCharacterAspas(Character ch) {
 		return ch == '"' || ch == '\'';
 	}
-	
-	protected static Boolean isCharacterEspaco(Character ch){
+
+	protected static Boolean isCharacterEspaco(Character ch) {
 		return ch == ' ';
 	}
 
 	protected static Boolean isCharacterIgnorado(Character ch) {
-		return ch == ' ' || ch == '\n' || ch == '\t' || ch=='\r' ;
+		return ch == ' ' || ch == '\n' || ch == '\t' || ch == '\r';
 	}
-        
+
 	protected static Boolean isSeparador(String lex) {
 		return Arrays.asList(SEPARADORES).contains(lex);
 	}
